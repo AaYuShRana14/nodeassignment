dfdb9ce279cc600c5b5a6c160439b48d7cbdf0d7
AaYuShRana14 <antineutrino1464@gmail.com>
2024-03-07 19:29:50 +0530

    added comments

--- index.js
+++ index.js
@@ -33,16 +33,16 @@ mongoose.connect('mongodb+srv://antineutrino1464:tvEdj7tV3HY@cluster.4ipxphf.mon
 
 app.post('/register',async(req,res)=>{
     const {email,username,password}=req.body;
-    const data=userdata.safeParse({email,username,password});
+    const data=userdata.safeParse({email,username,password}); // validating input fields
     if(data.success){
         const user=await Users.findOne({email});
         if(user){
-            res.status(403).send({message:"User already exists"});
+            res.status(403).send({message:"User already exists"}); // not letting to create a user if the email is used
             return;
         }
         else{
-            const hashpassword = await bcrypt.hash(password, 10);
-            const newUser=new Users({email,username,password:hashpassword});
+            const hashpassword = await bcrypt.hash(password, 10); // hashing the password before storing in database
+            const newUser=new Users({email,username,password:hashpassword}); // create new user
             await newUser.save();
             res.status(200).json({message:'registered successfully'});
             return;
@@ -53,14 +53,14 @@ app.post('/register',async(req,res)=>{
 });
 app.post('/login',async(req,res)=>{
     const{username,password}=req.body;
-    const data=logincredentials.safeParse({username,password});
+    const data=logincredentials.safeParse({username,password});// validating input fields
     if(data.success){
-        const user=await Users.findOne({username});
+        const user=await Users.findOne({username}); // finding if username exists in database
         if(!user){
             return res.status(401).json({error:"Invalid credentials"});
         }
         const passwordMatched = await bcrypt.compare(password, user.password);
-        if(!passwordMatched){
+        if(!passwordMatched){ //checking whether the provided plain text password matches the hashed password stored
             return res.status(401).json({error:"Invalid credentials"});
         }
         res.status(200).json({"message":"logged in successfully"});
@@ -80,29 +80,29 @@ app.post('/forgot-password',async(req,res)=>{
     }
     const secret=jwtsecret+user.password;
     const userid=user._id.toString();
-    const token=jwt.sign({userid},secret, { expiresIn: '5m' });
+    const token=jwt.sign({userid},secret, { expiresIn: '5m' }); // creating a new token for reseting password link
     const resetpasswordlink=`http://localhost:3000/reset-password/${userid}/${token}`;
     return res.status(200).json({resetpasswordlink});
 })
-app.get('/reset-password/:id/:token',async(req,res)=>{
+app.get('/reset-password/:id/:token',async(req,res)=>{ // the link wont be valid after one time as the secret used will change.
     const{id,token}=req.params;
     const{password}=req.body;
-    if (!ObjectId.isValid(id)) {
+    if (!ObjectId.isValid(id)) { // checking if the user id is valid
         res.status(400).send('Invalid user ID');
         return;
     }
-    const user=await User.findById(id);
+    const user=await User.findById(id); // finding user in the database using userid
 
     if(!user){
         res.send("Invalid user");
         return;
     }
-    const secret=jwtsecret+user.password;
+    const secret=jwtsecret+user.password; // recreating the secret used to create jwt token earlier
     try{
-        const verified=jwt.verify(token,secret);
-        const hashpassword = await bcrypt.hash(password, 10);
-        user.password=hashpassword;
-        user.save();
+        const verified=jwt.verify(token,secret);// verifying the token if link is used once and password is changed the token will change and link cant be used again
+        const hashpassword = await bcrypt.hash(password, 10);// hashing the password before storing in database
+        user.password=hashpassword;// upadting the password 
+        user.save();// storing new password
         res.send(user);
     }
     catch(error){
